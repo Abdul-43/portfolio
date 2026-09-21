c6c008890adb004a2e284d9557d4e08d2af9c758
Abdul-43 <abdulpnoit.ar@gmail.com>
2026-09-21 16:10:52 +0400

    FIX: Remove 1.5s delay on Navbar to load instantly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
       className={`nav-container ${scrolled ? "scrolled" : ""}`}
       initial={{ y: -80, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.6, delay: 1.5 }}
+      transition={{ duration: 0.4, delay: 0.1 }}
     >
       <div className="nav-inner">
         <a
